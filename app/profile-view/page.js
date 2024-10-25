25b789c376e0f6ab2ac92e52e4dd27f3ae1f94f7
Skyz03 <aakibdiscoo@gmail.com>
2024-10-25 10:19:19 +0545

    fix: update in the profile view page

--- app/profile-view/page.js
+++ app/profile-view/page.js
@@ -1,6 +1,7 @@
 'use client'
 import { Button } from 'primereact/button'
 import Image from 'next/image'
+
 export default function ProfileView() {
   return (
     <>
@@ -8,21 +9,90 @@ export default function ProfileView() {
         <Button
           label="Back to Editor"
           className="w-full border border-primary bg-white p-4 text-primary"
-        ></Button>
+        />
 
         <Button
           label="Share Link"
           className="w-full bg-primary p-4 text-white"
-        ></Button>
+        />
       </div>
 
-      <div className="mx-auto text-center text-black">
-        <Image className="mx-auto" src="" width="30" height="30"></Image>
+      <div className="mx-auto mt-4 text-center text-black">
+        <Image
+          className="mx-auto"
+          src=""
+          width="30"
+          height="30"
+          alt="Profile Picture"
+        />
 
         <h1>Ben Wright</h1>
         <p>ben@example.com</p>
       </div>
 
+      <div className="mt-4 flex flex-col gap-2">
+        {/* Reusable GitHub Button with Images */}
+        <Button
+          label={
+            <span className="flex w-full items-center justify-between">
+              <Image
+                src="assets/images/icon-github.svg"
+                alt="GitHub Icon"
+                width={20}
+                height={20}
+              />
+              <span className="mx-2">Github</span>
+              <Image
+                src="assets/images/icon-arrow-right.svg"
+                alt="Arrow Icon"
+                width={20}
+                height={20}
+              />
+            </span>
+          }
+          className="w-full bg-black p-4 text-white"
+        />
+        <Button
+          label={
+            <span className="flex w-full items-center justify-between">
+              <Image
+                src="assets/images/icon-github.svg"
+                alt="GitHub Icon"
+                width={20}
+                height={20}
+              />
+              <span className="mx-2">Github</span>
+              <Image
+                src="assets/images/icon-arrow-right.svg"
+                alt="Arrow Icon"
+                width={20}
+                height={20}
+              />
+            </span>
+          }
+          className="w-full bg-black p-4 text-white"
+        />
+        <Button
+          label={
+            <span className="flex w-full items-center justify-between">
+              <Image
+                src="assets/images/icon-github.svg"
+                alt="GitHub Icon"
+                width={20}
+                height={20}
+              />
+              <span className="mx-2">Github</span>
+              <Image
+                src="assets/images/icon-arrow-right.svg"
+                alt="Arrow Icon"
+                width={20}
+                height={20}
+              />
+            </span>
+          }
+          className="w-full bg-black p-4 text-white"
+        />
+      </div>
     </>
   )
 }
