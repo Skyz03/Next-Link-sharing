f00a8e20cbfc6ee3fbc89498c0567e45a654cf76
Skyz03 <aakibdiscoo@gmail.com>
2024-10-26 15:27:56 +0545

    starter page updated

--- app/page.js
+++ app/page.js
@@ -1,7 +1,84 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="text-2xl font-bold text-slateBlack">Welcome to MyApp</h1>
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-extrabold text-slate-800">
+        Welcome to MyApp
+      </h1>
+
+      <nav className="mt-8 flex flex-col items-start space-y-4">
+        <Link
+          href="/login"
+          className="flex items-center text-lg text-slate-700 hover:text-slate-900"
+        >
+          <Image
+            src="assets/images/icon-arrow-right.svg"
+            alt="Arrow Right"
+            width={20}
+            height={20}
+            className="mr-2 invert filter"
+          />
+          <span>Login</span>
+        </Link>
+
+        <Link
+          href="/register"
+          className="flex items-center text-lg text-slate-700 hover:text-slate-900"
+        >
+          <Image
+            src="/assets/images/icon-arrow-right.svg"
+            alt="Arrow Right"
+            width={20}
+            height={20}
+            className="mr-2 invert filter"
+          />
+          <span>Register</span>
+        </Link>
+
+        <Link
+          href="/starter"
+          className="flex items-center text-lg text-slate-700 hover:text-slate-900"
+        >
+          <Image
+            src="/assets/images/icon-arrow-right.svg"
+            alt="Arrow Right"
+            width={20}
+            height={20}
+            className="mr-2 invert filter"
+          />
+          <span>Starter</span>
+        </Link>
+
+        <Link
+          href="/profile"
+          className="flex items-center text-lg text-slate-700 hover:text-slate-900"
+        >
+          <Image
+            src="/assets/images/icon-arrow-right.svg"
+            alt="Arrow Right"
+            width={20}
+            height={20}
+            className="mr-2 invert filter"
+          />
+          <span>Profile</span>
+        </Link>
+
+        <Link
+          href="/profile-view"
+          className="flex items-center text-lg text-slate-700 hover:text-slate-900"
+        >
+          <Image
+            src="/assets/images/icon-arrow-right.svg"
+            alt="Arrow Right"
+            width={20}
+            height={20}
+            className="mr-2 invert filter"
+          />
+          <span>Profile View</span>
+        </Link>
+      </nav>
     </div>
   )
 }
