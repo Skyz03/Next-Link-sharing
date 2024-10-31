5c4100b60a5a3ec2fe8629f5fa4da35e5f30082f
Skyz03 <aakibdiscoo@gmail.com>
2024-10-31 11:30:43 +0545

    fix: css & styles

--- app/components/forms/SaveButton.js
+++ app/components/forms/SaveButton.js
@@ -4,9 +4,7 @@ export default function SaveButton({ onClick, disabled }) {
   return (
     <Button
       label="Save"
-      className={`mt-6 w-full rounded-md py-3 font-semibold text-pureWhite ${
-        disabled ? 'bg-primaryLight' : 'bg-primary'
-      }`}
+      className={`btn-primary ${disabled ? 'bg-primaryLight' : 'bg-primary'}`}
       onClick={onClick}
       disabled={disabled}
     />
--- app/starter/LinkCard.js
+++ app/starter/LinkCard.js
@@ -13,67 +13,69 @@ export default function LinkCard({
   dragHandleProps, // Pass dragHandleProps to the card
 }) {
   return (
-    <Card className="m-4 rounded-lg bg-offWhite shadow-sm">
-      <div className="mb-4 flex items-center justify-between">
-        <div
-          className="flex items-center gap-2"
-          {...dragHandleProps}
-          style={{ cursor: 'grab', userSelect: 'none' }}
-        >
-          {/* Drag icon now uses dragHandleProps */}
-          <Image
-            src="/assets/images/icon-drag-and-drop.svg"
-            width={12}
-            height={6}
-            alt="Drag and Drop Icon"
-          />
-          <h3 className="font-bold text-slateBlack">Link #{linkNumber}</h3>
+    <div className="m-4">
+      <Card className="m-4 rounded-lg bg-offWhite shadow-sm">
+        <div className="my-2 flex items-center justify-between">
+          <div
+            className="flex items-center gap-2"
+            {...dragHandleProps}
+            style={{ cursor: 'grab', userSelect: 'none' }}
+          >
+            {/* Drag icon now uses dragHandleProps */}
+            <Image
+              src="/assets/images/icon-drag-and-drop.svg"
+              width={12}
+              height={6}
+              alt="Drag and Drop Icon"
+            />
+            <h3 className="font-bold text-slateBlack">Link #{linkNumber}</h3>
+          </div>
+          <p
+            className="cursor-pointer text-midGray hover:underline"
+            onClick={onRemove}
+          >
+            Remove
+          </p>
         </div>
-        <p
-          className="cursor-pointer text-midGray hover:underline"
-          onClick={onRemove}
-        >
-          Remove
-        </p>
-      </div>
 
-      <div className="mb-4">
-        <label className="mb-2 block text-sm font-semibold text-black">
-          Platform
-        </label>
-        <PlatformDropdown
-          selectedPlatform={selectedPlatform}
-          onSelect={onSelectPlatform}
-        />
-      </div>
-
-      <div>
-        <label className="mb-2 flex items-center gap-2 text-sm font-semibold text-black">
-          <Image
-            src="/assets/images/icon-link.svg"
-            width={20}
-            height={20}
-            alt="Link Icon"
-          />
-          Link
-        </label>
-        <div className="flex items-center rounded-md border border-gray-300 bg-white p-2">
-          <Image
-            src="/assets/images/icon-link.svg"
-            width={20}
-            height={20}
-            alt="Link Icon"
-            className="mr-2"
-          />
-          <InputText
-            value={value}
-            onChange={onChange}
-            className="w-full text-black focus:shadow-none"
-            placeholder="Enter your link"
-            variant="outlined"
+        <div className="mb-4">
+          <label className="mb-2 block text-sm font-semibold text-black">
+            Platform
+          </label>
+          <PlatformDropdown
+            selectedPlatform={selectedPlatform}
+            onSelect={onSelectPlatform}
           />
         </div>
-      </div>
-    </Card>
+
+        <div>
+          <label className="mb-2 flex items-center gap-2 text-sm font-semibold text-black">
+            <Image
+              src="/assets/images/icon-link.svg"
+              width={20}
+              height={20}
+              alt="Link Icon"
+            />
+            Link
+          </label>
+          <div className="flex items-center rounded-md border border-gray-300 bg-white p-2">
+            <Image
+              src="/assets/images/icon-link.svg"
+              width={20}
+              height={20}
+              alt="Link Icon"
+              className="mr-2"
+            />
+            <InputText
+              value={value}
+              onChange={onChange}
+              className="w-full text-black focus:shadow-none"
+              placeholder="Enter your link"
+              variant="outlined"
+            />
+          </div>
+        </div>
+      </Card>
+    </div>
   )
 }
